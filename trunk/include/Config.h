ace60f82e87a72ffee6e2f8b3c6e898b1327c519
Vijayakanth <vijayakanth@vijayakanth-HP430.(none)>
2013-04-29 17:18:03 +0530

    adding Config.h

---
+++ trunk/include/Config.h
@@ -0,0 +1,8 @@
+#include <ubac/YamlConfig.h>
+#include "ConnectionDetails.h"
+#include <ubac/Mutex.h>
+#include <ubac/SLog.h>
+extern Mutex log_mutex;
+extern SLog *logger;
+extern YamlConfig *yamlConfig;
+extern ConnectionDetails *database_config;
